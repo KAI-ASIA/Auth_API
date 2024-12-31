eb349e5fd22833a65d166a7f5d89378063adecbe
Nguyen Tuan Vinh Ninh K17 HL <ninhntvhe172162@fpt.edu.vn>
2024-12-31 17:47:29 +0700

    upadate confirmOTPService process

--- src/main/java/com/kaiasia/app/service/Auth_api/api/api_confirmOTP/ConfirmOTPService.java
+++ src/main/java/com/kaiasia/app/service/Auth_api/api/api_confirmOTP/ConfirmOTPService.java
@@ -8,16 +8,18 @@ import com.kaiasia.app.core.utils.GetErrorUtils;
 import com.kaiasia.app.register.KaiMethod;
 import com.kaiasia.app.register.KaiService;
 import com.kaiasia.app.register.Register;
+import com.kaiasia.app.service.Auth_api.dao.IAuthOTPService;
+import com.kaiasia.app.service.Auth_api.dao.imp.AuthOTPService;
+import com.kaiasia.app.service.Auth_api.model.Auth3Response;
+import com.kaiasia.app.service.Auth_api.model.OTP;
 import com.kaiasia.app.service.Auth_api.utils.ApiHelper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
+import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,6 +30,8 @@ public class ConfirmOTPService {
     @Autowired
     GetErrorUtils apiErrorUtils;
 
+    @Autowired
+    private IAuthOTPService authOTPService;
 
     @KaiMethod(name = "confirmOTP", type = Register.VALIDATE)
     public ApiError validate(ApiRequest req) {
@@ -40,7 +44,6 @@ public class ConfirmOTPService {
         }
         System.out.println("Bat dau in");
         System.out.println(enquiry);
-
         String[] requiredFields = new String[]{
                 "sessionId", "username", "otp",
                 "transTime", "transId"
@@ -49,7 +52,7 @@ public class ConfirmOTPService {
         for (String requiredField : requiredFields) {
             if (!enquiry.containsKey(requiredField)
                     || StringUtils.isEmpty((String) enquiry.get(requiredField))) {
-                return apiErrorUtils.getError("404", new String[]{requiredFields + " is required!"});
+                return apiErrorUtils.getError("706", new String[]{requiredFields + " is required!"});
             }
         }
         return new ApiError(ApiError.OK_CODE, ApiError.OK_DESC);
@@ -67,21 +70,36 @@ public class ConfirmOTPService {
 //        ApiResponse sesionID = callAPI.call("", HttpMethod.POST , httpHeaders, );
 
 
-
-        ApiResponse apiResponse = new ApiResponse();
-
-
-        ApiBody apiBody = new ApiBody();
+        Auth3Response auth3Response = new Auth3Response();
+        auth3Response.setResponseCode("00");
+        auth3Response.setTransId(enquiry.get("transId").toString());
+        OTP otp = new OTP();
         try {
-            Map<String, Object> bodyEnq = new HashMap<>();
-
-            apiResponse.setBody(apiBody);
-            return apiResponse;
+            otp = authOTPService.getOTP(enquiry);
         } catch (Exception e) {
-            log.error("Exception", e);
-            ApiError apiError = apiErrorUtils.getError("999", new String[]{e.getMessage()});
-            apiResponse.setError(apiError);
-            return apiResponse;
+            e.printStackTrace();
+
         }
+
+        //Check time out cua ma OTP
+        if (authOTPService.checkTimeOut(otp.getEnd_time())) {
+            ApiError apiError = apiErrorUtils.getError("998", new String[]{"OTP expired!"});
+            return authOTPService.takeRespose(auth3Response, apiError);
+        }
+
+        //Check xem ma OTP co dung khong
+        if (enquiry.get("otp").toString().equals(otp.getValidate_code())) {
+            Timestamp now = new Timestamp(System.currentTimeMillis());
+            try {
+                authOTPService.setConfirmTime(now, otp);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return authOTPService.takeRespose(auth3Response);
+        }
+
+        //Neu khong dung thi tra ve loi sai otp
+        ApiError apiError = apiErrorUtils.getError("605", new String[]{"Wrong OTP!"});
+        return authOTPService.takeRespose(auth3Response, apiError);
     }
 }
