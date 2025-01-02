63f78bd86f44728db114b24cdfd324ccfc37c5d0
Nguyen Tuan Vinh Ninh K17 HL <ninhntvhe172162@fpt.edu.vn>
2025-01-02 15:34:20 +0700

    Auth_api/ultils/ServiceUltil update: update comment for 2 method takeResponse

--- src/main/java/com/kaiasia/app/service/Auth_api/utils/ServiceUltil.java
+++ src/main/java/com/kaiasia/app/service/Auth_api/utils/ServiceUltil.java
@@ -12,11 +12,11 @@ import java.sql.Timestamp;
 public class ServiceUltil {
 
     /**
-     *
-     * @param response
-     * @param error
-     * @return
-     * @param <T>
+     * Trả về 1 response khi api chạy có lỗi
+     * @param response Giữ liệu trả về
+     * @param error Lỗi được truyền vào để thêm vào response
+     * @return Trả về 1 ApiResponse bao gồm 1 apiBody có kiểu giữ liệu trả về và 1 ApiError
+     * @param <T> Kiểu giữ liệu được chuyền vào để cho vào apiBody
      */
     public static <T> ApiResponse takeRespose( T response, ApiError error) {
         ApiResponse apiResponse = new ApiResponse();
@@ -27,6 +27,12 @@ public class ServiceUltil {
         return apiResponse;
     }
 
+    /**
+     * Trả về 1 response khi api chay không có lỗi
+     * @param response Giữ liệu trả về
+     * @return Trả về 1 ApiResponse bao gồm 1 apiBody có kiểu giữ liệu trả về
+     * @param <T> Kiểu giữ liệu được chuyền vào để cho vào apiBody
+     */
     public static <T> ApiResponse takeRespose(T response) {
         ApiResponse apiResponse = new ApiResponse();
         ApiBody apiBody = new ApiBody();
