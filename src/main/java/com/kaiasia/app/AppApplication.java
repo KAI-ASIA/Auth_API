f291d08029cadab62eef9c86fd6fbce9b519cee8
vietloc2901 <nvloc29012000@gmail.com>
2025-01-07 22:10:13 +0700

    Update login

--- src/main/java/com/kaiasia/app/AppApplication.java
+++ src/main/java/com/kaiasia/app/AppApplication.java
@@ -2,10 +2,13 @@ package com.kaiasia.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource({"classpath:spring-beans-dao.xml"})
+@ComponentScan(basePackages = {"com.kaiasia.app", "ms.apiclient.client"})
 public class AppApplication {
 
     public static void main(String[] args) {
