e675622c17d3738e3e20afd75170a4bf4acd179f
tuanhoang23 <nguyenhoang892001@gmail.com>
2025-01-09 13:07:17 +0700

    validate request

--- src/main/java/com/kaiasia/app/service/Auth_api/api/resetpassword/ResetPasswordRequestService.java
+++ src/main/java/com/kaiasia/app/service/Auth_api/api/resetpassword/ResetPasswordRequestService.java
@@ -8,11 +8,15 @@ import com.kaiasia.app.core.model.ApiRequest;
 import com.kaiasia.app.core.utils.GetErrorUtils;
 import com.kaiasia.app.register.KaiMethod;
 import com.kaiasia.app.register.KaiService;
+import lombok.extern.slf4j.Slf4j;
 import ms.apiclient.t24util.T24UtilClient;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.HashMap;
+
 @KaiService
+@Slf4j
 public class ResetPasswordRequestService {
 
     @Autowired
@@ -26,8 +30,32 @@ public class ResetPasswordRequestService {
 
     @KaiMethod
     public ApiError validate(ApiRequest req) throws Exception {
-        String LOCATION = "";
 
-        return null;
+        HashMap enquiry = (HashMap) req.getBody().get("enquiry");
+        String channel = req.getHeader().getChannel();
+        String username = (String) enquiry.get("username");
+        String transId = (String) enquiry.get("transId");
+        long time = System.currentTimeMillis();
+        String location = channel +"-"+ username +"-"+ transId +"-"+ time;
+        
+        if (req.getBody() == null) {
+            log.info("#BODY NULL" + location);
+            return apiErrorUtils.getError("804", new String[]{"Missing request body!"});
+        }
+        if (enquiry == null) {
+            log.info("#ENQUIRY NULL" + location);
+            return apiErrorUtils.getError("804", new String[]{"Missing enquiry part!"});
+        }
+
+        if(StringUtils.isBlank(username)){
+            log.info("#FIELD NAME NULL" + location);
+            return apiErrorUtils.getError("804", new String[]{"Missing field name !"});
+        }
+        if(StringUtils.isBlank(transId)){
+            log.info("#FIELD TRANSID NULL" + location);
+            return apiErrorUtils.getError("804", new String[]{"Missing field transId !"});
+        }
+
+        return new ApiError(ApiError.OK_CODE, ApiError.OK_DESC);
     }
 }
