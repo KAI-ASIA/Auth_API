1c2b55ad32c8cdde985e7beba4695c70af3692b8
Hung138k <vhungk7@gmail.com>
2025-01-10 16:57:54 +0700

    Doing getOTP send to Kafka

--- src/main/java/com/kaiasia/app/service/Auth_api/api/api_getOTP/GetOTPService.java
+++ src/main/java/com/kaiasia/app/service/Auth_api/api/api_getOTP/GetOTPService.java
@@ -4,18 +4,17 @@ import com.kaiasia.app.core.model.ApiBody;
 import com.kaiasia.app.core.model.ApiError;
 import com.kaiasia.app.core.model.ApiRequest;
 import com.kaiasia.app.core.model.ApiResponse;
+import com.kaiasia.app.core.utils.ApiConstant;
 import com.kaiasia.app.core.utils.GetErrorUtils;
 import com.kaiasia.app.register.KaiMethod;
 import com.kaiasia.app.register.KaiService;
 import com.kaiasia.app.register.Register;
 import com.kaiasia.app.service.Auth_api.dao.IAuthOTPDao;
-import com.kaiasia.app.service.Auth_api.model.Auth2Response;
-import com.kaiasia.app.service.Auth_api.model.OTP;
+import com.kaiasia.app.service.Auth_api.dto.GetOTPResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import static com.kaiasia.app.service.Auth_api.utils.ServiceUltil.takeRespose;
 
@@ -76,12 +75,14 @@ public class GetOTPService {
     @KaiMethod(name = "getOTP")
     public ApiResponse process(ApiRequest req) {
         HashMap enquiry = (HashMap) req.getBody().get("enquiry");
-        log.info("Body print:");
+        Long a = System.currentTimeMillis();
+        String LOCATION = "GetOTP" + enquiry.get("sessionId");
+
+        log.info(LOCATION + "#BEGIN");
+
+        ApiResponse apiResponse = new ApiResponse();
 
 
-        Auth2Response auth2Response = new Auth2Response();
-        auth2Response.setResponseCode("00");
-        auth2Response.setTransId(enquiry.get("transId").toString());
 
         try {
             String generateOTP = authOTPService.generateAndSaveOTP(enquiry);
@@ -90,13 +91,24 @@ public class GetOTPService {
 
             if (!isOTPValid) {
                 ApiError apiError = apiErrorUtils.getError("605", new String[]{"Wrong OTP!"});
-                return takeRespose(auth2Response, apiError);
+                apiResponse.setError(apiError);
+                log.info(LOCATION + "#END#Duration:" + (System.currentTimeMillis() - a));
+                return apiResponse;
             }
-            return takeRespose(auth2Response, null);
+            GetOTPResponse response = new GetOTPResponse();
+            response.setResponseCode("00");
+            response.setTransId(enquiry.get("transId").toString());
+            ApiBody apiBody = new ApiBody();
+            apiBody.put(ApiConstant.COMMAND.ENQUIRY, response);
+            apiResponse.setBody(apiBody);
+
         } catch (Exception e) {
-            ApiError apiError = apiErrorUtils.getError("804", new String[]{"OTP is not exists!"});
-            return takeRespose(auth2Response, apiError);
+            log.error("{}:{}",LOCATION,e.getMessage());
+            ApiError apiError = apiErrorUtils.getError(ApiConstant.ErrorCode.INTERNAL_SERVER_ERROR, new String[] {e.getMessage()});
+            apiResponse.setError(apiError);
         }
+        log.info(LOCATION + "#END#Duration:" + (System.currentTimeMillis() - a));
+        return apiResponse;
 
     }
 }
--- src/main/java/com/kaiasia/app/service/Auth_api/dao/imp/AuthOTPDao.java
+++ src/main/java/com/kaiasia/app/service/Auth_api/dao/imp/AuthOTPDao.java
@@ -24,6 +24,8 @@ public class AuthOTPDao implements IAuthOTPDao {
     @Autowired
     PosgrestDAOHelper posgrestDAOHelper;
 
+//    @Autowired
+//    private KafkaTemplate<String, String> kafkaTemplate;
 
     @Override
     public OTP getOTP(HashMap<String, String> body) throws Exception {
@@ -53,8 +55,8 @@ public class AuthOTPDao implements IAuthOTPDao {
 
     @Override
     public boolean compareOTPAndCheckExpiration(HashMap<String, String> body) throws Exception {
-        String sql = "SELECT * FROM OTP  " +
-                "WHERE session_id = :session_id AND channel = :channel AND username = :username  ORDER BY trans_id DESC";
+        String sql = "SELECT * FROM auth_api.otp  " +
+                "WHERE auth_api.otp.session_id = :session_id AND auth_api.otp.channel = :channel AND auth_api.otp.username = :username  ORDER BY auth_api.otp.trans_id DESC";
         Map<String, String> params = new HashMap<>();
         params.put("session_id", body.get("session_id"));
         params.put("channel", body.get("channel"));
@@ -70,7 +72,7 @@ public class AuthOTPDao implements IAuthOTPDao {
     }
 
     @Override
-    public String generateAndSaveOTP(HashMap<String, String> body) {
+    public String generateAndSaveOTP(HashMap<String, String> body) throws Exception{
         String otp = generateOTP();
         OTP newOTP = new OTP();
         newOTP.setSession_id(body.get("session_id"));
@@ -79,8 +81,30 @@ public class AuthOTPDao implements IAuthOTPDao {
         newOTP.setValidate_code(otp);
         newOTP.setEnd_time(Timestamp.valueOf(LocalDateTime.now().plusMinutes(2)));
 
-//        kafkaTemplate.send("otp-topic", otp);
-        return otp;
+        String sql = "INSERT INTO auth_api.otp\n" +
+                "(validate_code, trans_id, username, channel, start_time, \"location\", end_time, status, session_id, confirm_time, trans_info, trans_time)\n" +
+                "VALUES(:validate_code, :trans_id, :username,:channel, :start_time, :location,:end_time, :status, :session_id, :confirm_time, :trans_info, :trans_time);";
+        HashMap<String, Object> param = new HashMap();
+        param.put("validate_code", newOTP.getValidate_code());
+        param.put("trans_id", newOTP.getTrans_id());
+        param.put("username", newOTP.getUsername());
+        param.put("channel", newOTP.getChannel());
+        param.put("start_time", newOTP.getStart_time());
+        param.put("location", newOTP.getLocation());
+        param.put("end_time", newOTP.getEnd_time());
+        param.put("status", newOTP.getStatus());
+        param.put("session_id", newOTP.getSession_id());
+        param.put("confirm_time",newOTP.getConfirm_time());
+        param.put("trans_info", newOTP.getTrans_info());
+        param.put("trans_time",newOTP.getTrans_time());
+
+        int result = posgrestDAOHelper.update(sql, param);
+        if (result > 0){
+            //        kafkaTemplate.send("otp-topic", otp);
+            return otp;
+        }
+
+        return null;
     }
 
     private String generateOTP() {
